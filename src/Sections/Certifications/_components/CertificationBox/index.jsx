397dad375674fcfef0901241661f51447fcd8a6a
omar abelrhman <omar.abdelrahman@matnsolutions.com>
2024-11-15 14:38:22 +0200

    certfications

--- src/Sections/Certifications/_components/CertificationBox/index.jsx
+++ src/Sections/Certifications/_components/CertificationBox/index.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import styles from './.module.scss'
 
-const CertificationBox = ({ image, title, date }) => {
+const CertificationBox = ({ image, title, subtitle, date }) => {
   return (
     <div className={styles.box}>
       <div className={styles.image}>
         <img
-          src="https://images.unsplash.com/file-1695862006750-2a3aca1bfdfaimage?w=416&dpr=2&auto=format&fit=crop&q=60"
-          alt=""
+          src={image}
+          alt="certification__image"
         />
       </div>
       <div className={styles.text__content}>
         <h3 className={`orange__text ${styles.box__title}`}>
-          الإجازة في الحديث الشريف
+          {title}
         </h3>
         <p className={`gray__text ${styles.text}`}>
-          هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا
-          النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من
-          النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+          {subtitle}
         </p>
-        <span className={`gray__text ${styles.date}`}>24/2024</span>
+        <span className={`gray__text ${styles.date}`}>{date}</span>
       </div>
     </div>
   );
